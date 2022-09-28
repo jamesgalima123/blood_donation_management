dc5a8346bb8fd4b2c14d8ab223b7f2e08da8880d
jamesgalima12321 <cjaygalima@gmail.com>
2022-09-28 11:48:48 +0800

    fix in patient sign up

--- patient/models.py
+++ patient/models.py
@@ -12,7 +12,7 @@ class Patient(models.Model):
 
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=False)
-   
+
     @property
     def get_name(self):
         return self.user.first_name+" "+self.user.last_name
