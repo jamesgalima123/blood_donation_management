72566acc5786dfcc709c2555efce21e5636b68fa
unknown <jamesgalima12321@gmail.com>
2022-12-13 17:16:22 +0800

    added email

--- blood/views.py
+++ blood/views.py
@@ -15,7 +15,15 @@ from donor import forms as dforms
 from patient import forms as pforms
 import logging
 import json
-
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+mail_content = '''Hello,
+This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library. Thank You'''
+#The mail addresses and password
+sender_address = 'desxpert.app@gmail.com'
+sender_pass = 'uneffbeoboeazsfq'
+receiver_address = 'receiver567@gmail.com'
 def home_view(request):
     x=models.Stock.objects.all()
     if len(x)==0:
@@ -325,7 +333,21 @@ def admin_donation_view(request):
 
 @login_required(login_url='adminlogin')
 def update_approve_status_view(request,pk,units):
-    print("frag")
+    # Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'A test mail sent by Python. It has an attachment.'  # The subject line
+    # The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    # Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+    session.starttls()  # enable security
+    session.login(sender_address, sender_pass)  # login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
     req=models.BloodRequest.objects.get(id=pk)
     message=None
     bloodgroup=req.bloodgroup
